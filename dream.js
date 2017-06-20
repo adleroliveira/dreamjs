7afa4aa2117575da791f06b60137e3a0c7760b81
krasimir <krasimir@localhost.localdomain>
2017-06-20 14:14:26 +0300

    Add support for lodash 4

--- dream.js
+++ dream.js
@@ -287,7 +287,12 @@ function Dream() {
     if (isValidSchema(schema)) return schema;
 
     if (typeof (schema) === 'string') {
-      var foundSchema = _.findWhere(_schemas, { name: schema });
+      var foundSchema = null;
+      if(_.findWhere) { // lodash 3
+        foundSchema =  _.findWhere(_schemas, { name: schema });
+      } else { // lodash 4
+        foundSchema =  _.find(_schemas, { name: schema });
+      }
       return isValidSchema(foundSchema) ? foundSchema : _genericSchema;
     }
 
